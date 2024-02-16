7d75b978bda735c80abaaf1ac99c155785d4f8db
thuan <thuanducn94@gmail.com>
2024-02-16 15:23:51 +0100

    implement signup

--- api/endpoints/users.js
+++ api/endpoints/users.js
@@ -30,4 +30,30 @@ router.post('/login', (req, res) => {
     return res.json({ ...user, profiles });
 });
 
+router.post('/signup', (req, res) => {
+    const { email, password, name, lastname, zip, city } = req.body;
+    const user = data.find((i) => i.email === email);
+
+    if(user) {
+        return res.json({ error: { message: "Email is already in use", body: req.body }});
+    }
+    if(!email || !password || !name || !lastname || !zip || !city) {
+        return res.json({ 
+            error: { message: "Missing values is already in use", 
+            body: { 
+                email: email || 'missing', 
+                password: password || 'missing', 
+                name: name || 'missing', 
+                lastname: lastname || 'missing', 
+                zip: zip || 'missing', 
+                city: city || 'missing',
+            } 
+        }});
+    }
+
+    const newUser = { email, password, name, lastname, zip, city, profiles: [], active: true, status: "WAITING" };
+    data.push(newUser)
+    return res.json(newUser);
+});
+
 module.exports = router;
