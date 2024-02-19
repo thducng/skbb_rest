273866e8d9e49255f83486574f737e5af7e43957
thuan <thuanducn94@gmail.com>
2024-02-19 20:38:12 +0100

    add access control for everywhere

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const api = require('./api');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(express.json());
 
 app.use('/api', api);
