7b0ccf0627b0724efa2bd6903c87bda58224b6bb
thuan <thuanducn94@gmail.com>
2024-08-01 12:37:16 +0200

    rollback to before vercel

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const server = require('./server');
 const PORT = process.env.PORT || 8080;
+const express = require('express');
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+const api = require('./api');
+const server = express();
+const cors = require('cors');
+
+const corsOpts = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}
+
+const options = {
+    info: {
+      version: '1.0.0',
+      title: 'Specifik Kidz Breakverse',
+      license: {
+        name: 'MIT',
+      },
+    },
+    filesPattern: './**/*.js',
+    baseDir: __dirname
+  };
+expressJSDocSwagger(server)(options);
+
+server.use(cors(corsOpts));
+server.use(express.json());
+
+server.use('/api', api);
+server.use('/images', express.static('images'));
+
+module.exports = server;
 
 server.listen(
     PORT,
