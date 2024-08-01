049528d6efdc2b3d3010c26cdcd31acdd2229d20
thuan <thuanducn94@gmail.com>
2024-08-01 12:30:37 +0200

    refactor server

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
-const express = require('express');
-const expressJSDocSwagger = require('express-jsdoc-swagger');
-const api = require('./api');
-const app = express();
-const PORT = process.env.PORT || 8080;
-const cors = require('cors');
+const server = require('./server');
 
-const corsOpts = {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
-}
-
-const options = {
-    info: {
-      version: '1.0.0',
-      title: 'Specifik Kidz Breakverse',
-      license: {
-        name: 'MIT',
-      },
-    },
-    filesPattern: './**/*.js',
-    baseDir: __dirname
-  };
-expressJSDocSwagger(app)(options);
-
-app.use(cors(corsOpts));
-app.use(express.json());
-
-app.use('/api', api);
-app.use('/images', express.static('images'));
-
-app.listen(
+server.listen(
     PORT,
     () => console.log('Running on PORT: ' + PORT)
 );
